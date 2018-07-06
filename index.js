09e500253337f094531af60edbdeed42580e432c
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-06 17:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+// add solution
+function theBeatlesPlay(musicians, instruments) {
+  var allMusicians = [];
+  for (var i = 0; i < musicians.length; i++) {
+  allMusicians.push(musicians[i] + "plays" + instruments[i]);
+}
+return allMusicians;
+}
